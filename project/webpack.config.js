59b118d9a52b5c44e4c7e395b54d585f9d985aef
DariaKarpovich <mood@gmail.com>
2020-02-29 23:20:17 +0300

    change gitignore and webpackConfig

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
     devServer: {
-        contentBase: './dist',
-        overlay: true
+        port: 8080,
+        compress: true,
+        contentBase: path.join(__dirname, 'dist'),
+        overlay: true,
+
     }
+
 }
