376a73d45ae000519dc87a738c111d32a104fc4d
DariaKarpovich <mood@gmail.com>
2020-03-09 12:51:48 +0300

    changes in config

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,70 @@
 const path = require('path');
 const WebpackBar = require('webpackbar');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output:{
-        path: path.resolve(__dirname, './dist'),
-        filename: 'main.js'
-    },
-    context: path.resolve(__dirname, 'src'),
-    resolve: {
-        extensions: ['.wasm', '.mjs', '.js', '.json']
+
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  context: path.resolve(__dirname, 'src'),
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+
+  stats: {
+    colors: true,
+  },
+
+  devServer: {
+    port: 8080,
+    compress: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    overlay: true,
+  },
+
+  module: {
+    rules: [
+
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
       },
 
-    devServer: {
-        port: 8080,
-        compress: true,
-        contentBase: path.join(__dirname, 'dist'),
-        overlay: true,
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-
-    stats: {
-        colors: true
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
 
-    module: {
-        rules: [
+      {
+        test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|svg)$/,
+        use: ['url-loader'],
+        exclude: /node_modules/,
+      },
 
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
+    ],
+  },
 
-            {
-                test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|svg)$/,
-                use: ['url-loader'],
-                exclude: /node_modules/,
-            },
-
-        ]
-    },
-
-    plugins: [
-        new WebpackBar(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html')
-        }),
-        
-    ]
-
-
-
-}
+  plugins: [
+    new WebpackBar(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'PokemonAPI',
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+};
